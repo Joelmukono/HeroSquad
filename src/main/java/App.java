7e266db8591245a831fdafa4258d3f3551011ed0
Joelmukono <jlmukono@gamil.com>
2020-01-06 10:38:06 +0300

    add main java class App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,14 @@
+import models.Hero;
+import models.Squad;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
 }
